14e95eb0171536cc50e19a80785ca29c6c92bbb2
Nathan OGER <github.sultry568@passmail.net>
2025-10-10 10:34:34 +0200

    Couleur de fond plus marqué

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
+      <body className="antialiased bg-gray-100 min-h-screen">
         <Providers>
           <Navbar />
           {children}
